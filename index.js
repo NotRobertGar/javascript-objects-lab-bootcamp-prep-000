38a03645890321644efa6514d21a550fc6800c1e
cab driver <cocktailsthecomic@gmail.com>
2017-09-04 20:54:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
   var newRecipes = Object.assign({}, object);
-  return newRecipes 
+  return newRecipes
+  return recipes;
 }
